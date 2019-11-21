b2d80c666de0d6ca61f9f3510a1c445ff2cc8cf6
jharte5 <jonathan.harte@codeimmersives.com>
2019-11-21 15:41:36 -0500

    real 1st commit

---
+++ back-end.js
@@ -0,0 +1,52 @@
+const biggify = function(num) {
+    return num += 9000
+}
+
+const nasafy = function(num) {
+    const results = []
+    let now = num
+    while (1 <= now) {
+    results.push(now);
+    now--;
+    } 
+    results.push('Blast off!')
+    return results
+}
+
+const reversify = function(str) {
+    return str.split('').reverse().join('')
+}
+
+const titleify = function(str) {
+    let titleCasedWords = [];
+    for (const word of str.split(' ')) {
+    titleCasedWords.push(word[0].toUpperCase() + word.slice(1).toLowerCase());
+    }
+
+    return titleCasedWords.join(' ');
+}
+
+const crazify = function(str) {
+    let crazified = '';
+    let crazyI = 0;
+    for (const character of str) {
+if (character === ' ') {
+        crazified += ' ';
+    } else {
+        crazified += crazyI % 2 === 1
+        ? character.toUpperCase()
+        : character.toLowerCase();
+        crazyI++;
+    }
+    }
+
+    return crazified;
+    
+}
+module.exports = {
+    biggify,
+    nasafy,
+    reversify,
+    titleify,
+    crazify,
+}
